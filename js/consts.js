b746b1f60ffb43951116530a53b9c019df515447
Omri Ossie <ocp1006@gmail.com>
2023-10-05 00:15:31 +0300

    Set colors as constants.

--- js/consts.js
+++ js/consts.js
@@ -1,21 +1,26 @@
 DEFAULT_COLOR = "#f8b000"
+BRONZE_COLOR = "#c29c6a"
+SILVER_COLOR = "#b5cccc"
+GOLD_COLOR = "#f9d000"
+PLATINUM_COLOR = "#7fdde8"
+DIAMOND_COLOR = "#3386ff"
 
 // Based on https://help.stake.com/en/articles/4793501-stake-vip-program-overview
 const vipRanksArray = [
     {rank: "None", value: 0, color: DEFAULT_COLOR},
-    {rank: "Bronze", value: 10_000, color: "#c29c6a"},
-    {rank: "Silver", value: 50_000, color: "#b5cccc"},
-    {rank: "Gold", value: 100_000, color: "#f9d000"},
-    {rank: "Platinum_I", value: 250_000, color: "#7fdde8"},
-    {rank: "Platinum_II", value: 500_000, color: "#7fdde8"},
-    {rank: "Platinum_III", value: 1_000_000, color: "#7fdde8"},
-    {rank: "Platinum_IV", value: 2_500_000, color: "#7fdde8"},
-    {rank: "Platinum_V", value: 5_000_000, color: "#7fdde8"},
-    {rank: "Platinum_VI", value: 10_000_000, color: "#7fdde8"},
-    {rank: "Diamond_I", value: 25_000_000, color: "#3386ff"},
-    {rank: "Diamond_II", value: 50_000_000, color: "#3386ff"},
-    {rank: "Diamond_III", value: 100_000_000, color: "#3386ff"},
-    {rank: "Diamond_IV", value: 250_000_000, color: "#3386ff"},
-    {rank: "Diamond_V", value: 500_000_000, color: "#3386ff"},
+    {rank: "Bronze", value: 10_000, color: BRONZE_COLOR},
+    {rank: "Silver", value: 50_000, color: SILVER_COLOR},
+    {rank: "Gold", value: 100_000, color: GOLD_COLOR},
+    {rank: "Platinum_I", value: 250_000, color: PLATINUM_COLOR},
+    {rank: "Platinum_II", value: 500_000, color: PLATINUM_COLOR},
+    {rank: "Platinum_III", value: 1_000_000, color: PLATINUM_COLOR},
+    {rank: "Platinum_IV", value: 2_500_000, color: PLATINUM_COLOR},
+    {rank: "Platinum_V", value: 5_000_000, color: PLATINUM_COLOR},
+    {rank: "Platinum_VI", value: 10_000_000, color: PLATINUM_COLOR},
+    {rank: "Diamond_I", value: 25_000_000, color: DIAMOND_COLOR},
+    {rank: "Diamond_II", value: 50_000_000, color: DIAMOND_COLOR},
+    {rank: "Diamond_III", value: 100_000_000, color: DIAMOND_COLOR},
+    {rank: "Diamond_IV", value: 250_000_000, color: DIAMOND_COLOR},
+    {rank: "Diamond_V", value: 500_000_000, color: DIAMOND_COLOR},
     {rank: "Obsidian", value: 1_000_000_000, color: DEFAULT_COLOR}
 ];
